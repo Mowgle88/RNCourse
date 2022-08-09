fe78674884d101b21bf6b20177e30d11387ece25
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-08-09 21:25:30 +0300

    feat: add a new property to the stylesheet object

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Hello World!</Text>
+        <Text style={styles.dummyText}>Hello World!</Text>
       </View>
-      <Text style={{margin: 16, borderWidth: 2, borderColor: 'red', padding:16}}>Hello, Dmitry!!!</Text>
+      <Text style={{margin: 8, borderWidth: 2, borderColor: 'red', padding:16}}>Hello, Dmitry!!!</Text>
       <Button title='Click me' />
     </View>
   );
@@ -20,4 +20,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin: 8,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: 'blue'
+  }
 });
